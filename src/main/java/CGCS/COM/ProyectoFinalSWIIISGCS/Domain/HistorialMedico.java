9d9684a03efb6af991160b91c4738dd205a5338c
Jhon Bravo <jbravol17_1@unc.edu.pe>
2024-03-02 01:09:16 -0500

    creando atributos de las calses indiviales

--- src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Domain/HistorialMedico.java
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Domain/HistorialMedico.java
@@ -1,4 +1,20 @@
 package CGCS.COM.ProyectoFinalSWIIISGCS.Domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class HistorialMedico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idHistorialMedico;
+    private String diagnostico;
+    private String alergias;
+    private String cirugias;
+    private String enfermedadesPrevias;
+    private String medicamentos;
 }
--- src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Domain/Horario.java
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Domain/Horario.java
@@ -1,10 +1,25 @@
 package CGCS.COM.ProyectoFinalSWIIISGCS.Domain;
-import jakarta.persistence.*;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-
+@Entity
+@Data
 public class Horario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idhorario;
+    // DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+    private LocalDate dia;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
 
 }
