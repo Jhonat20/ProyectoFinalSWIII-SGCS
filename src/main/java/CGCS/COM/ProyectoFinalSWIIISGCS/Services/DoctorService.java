d253ca1d6442b4dc06977873cb2f17cf5241f74e
Ivan Huaccha <rivahuaccha1.99@gmail.com>
2024-03-03 07:48:12 -0500

    Actualización primera de Service

--- src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Services/DoctorService.java
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Services/DoctorService.java
@@ -1,11 +1,6 @@
 package CGCS.COM.ProyectoFinalSWIIISGCS.Services;
 
-import CGCS.COM.ProyectoFinalSWIIISGCS.Domain.Doctor;
 import CGCS.COM.ProyectoFinalSWIIISGCS.exception.IllegalOperationException;
-
-import java.util.List;
-import java.util.Optional;
-
 public interface DoctorService {
 
     List<Doctor> listarDoctores() throws IllegalOperationException;
