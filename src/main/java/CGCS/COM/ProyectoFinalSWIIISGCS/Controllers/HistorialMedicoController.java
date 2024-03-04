0b49161065d56ecead3aac4bf3929e01c242c0aa
Jhon Bravo <jbravol17_1@unc.edu.pe>
2024-03-04 14:01:03 -0500

    Documentando historialmedico controller y el serviceImp

--- src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Controllers/HistorialMedicoController.java
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Controllers/HistorialMedicoController.java
@@ -4,11 +4,8 @@ import CGCS.COM.ProyectoFinalSWIIISGCS.Domain.HistorialMedico;
 import CGCS.COM.ProyectoFinalSWIIISGCS.Services.HistorialMedicoService;
 import CGCS.COM.ProyectoFinalSWIIISGCS.Validation.ValidationUtil;
 import CGCS.COM.ProyectoFinalSWIIISGCS.exception.IllegalOperationException;
-import CGCS.COM.ProyectoFinalSWIIISGCS.responses.CustomVerificacionResponse;
 import CGCS.COM.ProyectoFinalSWIIISGCS.responses.GlobalResponse;
-import CGCS.COM.ProyectoFinalSWIIISGCS.responses.SuccessResponse;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.NoResultException;
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +17,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Controlador REST para manejar las operaciones relacionadas con los historiales médicos.
+ */
 @RestController
 @RequestMapping("/api/v1/HistorialMedico")
 public class HistorialMedicoController {
@@ -27,20 +27,32 @@ public class HistorialMedicoController {
     @Autowired
     private HistorialMedicoService historialMedicoService;
 
+    /**
+     * Obtiene una lista de todos los historiales médicos.
+     *
+     * @return ResponseEntity con la lista de historiales médicos o un mensaje de error si no hay información disponible.
+     */
     @GetMapping
     public ResponseEntity<?> listarHistorialMedico() {
-       List<HistorialMedico> historialMedicos = historialMedicoService.listarHistorialMedico();
-            if (historialMedicos.isEmpty()) {
-                return ResponseEntity.ok(GlobalResponse.error("No hay información disponible para mostrar"));
-            } else {
-                return ResponseEntity.ok(GlobalResponse.ok(historialMedicos));
-            }
+        List<HistorialMedico> historialMedicos = historialMedicoService.listarHistorialMedico();
+        if (historialMedicos.isEmpty()) {
+            return ResponseEntity.ok(GlobalResponse.error("No hay información disponible para mostrar"));
+        } else {
+            return ResponseEntity.ok(GlobalResponse.ok(historialMedicos));
+        }
     }
 
+    /**
+     * Obtiene un historial médico por su ID.
+     *
+     * @param id ID del historial médico a buscar.
+     * @return ResponseEntity con el historial médico correspondiente o un mensaje de error si no se encuentra.
+     * @throws IllegalOperationException Si no se encuentra el historial médico con el ID proporcionado.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> obtenerHistorialMedico(@PathVariable Long id) throws IllegalOperationException {
         Optional<HistorialMedico> optionalHistorialMedico = historialMedicoService.BuscarPorId(id);
-        if(optionalHistorialMedico.isPresent()) {
+        if (optionalHistorialMedico.isPresent()) {
             HistorialMedico historialMedico = optionalHistorialMedico.get();
             return ResponseEntity.ok(GlobalResponse.ok(historialMedico));
         } else {
@@ -48,17 +60,34 @@ public class HistorialMedicoController {
         }
     }
 
+    /**
+     * Guarda un nuevo historial médico.
+     *
+     * @param historialMedico El historial médico a guardar.
+     * @param bindingResult   Resultado de la validación de entrada.
+     * @return ResponseEntity con el historial médico guardado o un mensaje de error si la validación falla.
+     * @throws IllegalOperationException Si la validación falla.
+     */
     @PostMapping
     public ResponseEntity<?> guardarHistorialMedico(@Valid @RequestBody HistorialMedico historialMedico, BindingResult bindingResult) throws IllegalOperationException {
         if (bindingResult.hasErrors()) {
             Map<String, String> errores = ValidationUtil.getValidationErrors(bindingResult);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errores);
-        }else {
+        } else {
             HistorialMedico nuevoHistorialMedico = historialMedicoService.Grabar(historialMedico);
             return ResponseEntity.ok(GlobalResponse.ok(nuevoHistorialMedico));
         }
     }
 
+    /**
+     * Actualiza un historial médico existente.
+     *
+     * @param id             ID del historial médico a actualizar.
+     * @param historialMedico El historial médico actualizado.
+     * @param bindingResult  Resultado de la validación de entrada.
+     * @return ResponseEntity con el historial médico actualizado o un mensaje de error si la validación falla o el historial médico no se encuentra.
+     * @throws IllegalOperationException Si la validación falla o el historial médico no se encuentra.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<?> actualizarHistorialMedico(@PathVariable Long id, @Valid @RequestBody HistorialMedico historialMedico, BindingResult bindingResult) throws IllegalOperationException {
         Optional<HistorialMedico> historialMedicoOptional = historialMedicoService.BuscarPorId(id);
@@ -73,13 +102,20 @@ public class HistorialMedicoController {
         } else {
             return ResponseEntity.ok(GlobalResponse.error("No se encontró el historial médico con el ID proporcionado"));
         }
-
     }
 
+    /**
+     * Actualiza parcialmente un historial médico existente.
+     *
+     * @param id      ID del historial médico a actualizar.
+     * @param cambios Mapa con los campos a actualizar y sus nuevos valores.
+     * @return ResponseEntity con el historial médico actualizado parcialmente o un mensaje de error si el historial médico no se encuentra.
+     * @throws IllegalOperationException Si el historial médico no se encuentra.
+     */
     @PatchMapping("/{id}")
-    public ResponseEntity<?> actualizarParcialHistorialMedico(@PathVariable Long id, @RequestBody Map<String, Object> cambios) throws IllegalOperationException{
+    public ResponseEntity<?> actualizarParcialHistorialMedico(@PathVariable Long id, @RequestBody Map<String, Object> cambios) throws IllegalOperationException {
         Optional<HistorialMedico> historialMedicoOptional = historialMedicoService.BuscarPorId(id);
-        if(historialMedicoOptional.isPresent()){
+        if (historialMedicoOptional.isPresent()) {
             HistorialMedico historialMedico = historialMedicoOptional.get();
             historialMedicoService.actualizarParcial(id, cambios);
             return ResponseEntity.ok(GlobalResponse.ok(historialMedico));
@@ -88,8 +124,13 @@ public class HistorialMedicoController {
         }
     }
 
-
-
+    /**
+     * Elimina un historial médico existente.
+     *
+     * @param id ID del historial médico a eliminar.
+     * @return ResponseEntity con un mensaje de éxito si el historial médico se elimina correctamente o un mensaje de error si no se encuentra.
+     * @throws IllegalOperationException Si el historial médico no se encuentra.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarHistorialMedico(@PathVariable Long id) throws IllegalOperationException {
         Optional<HistorialMedico> historialMedicoOptional = historialMedicoService.BuscarPorId(id);
--- src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Services/HistorialMedicoServiceImp.java
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/Services/HistorialMedicoServiceImp.java
@@ -3,7 +3,6 @@ package CGCS.COM.ProyectoFinalSWIIISGCS.Services;
 import CGCS.COM.ProyectoFinalSWIIISGCS.Domain.HistorialMedico;
 import CGCS.COM.ProyectoFinalSWIIISGCS.Repositories.HistorialMedicoRepository;
 
-import CGCS.COM.ProyectoFinalSWIIISGCS.exception.IllegalOperationException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,31 +12,115 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Implementación del servicio para manejar operaciones relacionadas con el historial médico.
+ */
 @Service
 public class HistorialMedicoServiceImp implements HistorialMedicoService {
 
     @Autowired
     private HistorialMedicoRepository histMedRep;
 
+    /**
+     * Recupera una lista de todos los historiales médicos.
+     *
+     * @return Lista de historiales médicos.
+     */
     @Override
     @Transactional
     public List<HistorialMedico> listarHistorialMedico() {
         return histMedRep.findAll();
     }
 
+    /**
+     * Busca un historial médico por su ID.
+     *
+     * @param id ID del historial médico a buscar.
+     * @return Optional que contiene el historial médico si se encuentra, de lo contrario, Optional vacío.
+     */
     @Override
     @Transactional(readOnly = true)
     public Optional<HistorialMedico> BuscarPorId(Long id) {
-        Optional<HistorialMedico> historialMedicoOptional = histMedRep.findById(id);
+        return histMedRep.findById(id);
+    }
+
+    /**
+     * Guarda un nuevo historial médico en la base de datos.
+     *
+     * @param historialMedico El historial médico a guardar.
+     * @return El historial médico guardado.
+     */
+    @Override
+    @Transactional
+    public HistorialMedico Grabar(HistorialMedico historialMedico) {
+        asignarValoresPredeterminados(historialMedico);
+        return histMedRep.save(historialMedico);
+    }
+
+    /**
+     * Actualiza un historial médico existente en la base de datos.
+     *
+     * @param id              ID del historial médico a actualizar.
+     * @param historialMedico El nuevo historial médico con los cambios.
+     * @return El historial médico actualizado.
+     * @throws EntityNotFoundException Si no se encuentra el historial médico con el ID especificado.
+     */
+    @Override
+    @Transactional
+    public HistorialMedico Actualizar(Long id, HistorialMedico historialMedico) {
+        Optional<HistorialMedico> historialMedicoOptional = obtenerHistorialMedicoExistente(id);
         if (historialMedicoOptional.isPresent()) {
-            return historialMedicoOptional;
+            HistorialMedico historialMedicoExistente = historialMedicoOptional.get();
+            actualizarCamposHistorialMedico(historialMedicoExistente, historialMedico);
+            return histMedRep.save(historialMedicoExistente);
         } else {
-            return Optional.empty();
+            throw new EntityNotFoundException("Historial médico no encontrado con ID: " + id);
         }
     }
 
+    /**
+     * Elimina un historial médico de la base de datos.
+     *
+     * @param id ID del historial médico a eliminar.
+     * @throws EntityNotFoundException Si no se encuentra el historial médico con el ID especificado.
+     */
     @Override
-    public HistorialMedico Grabar(HistorialMedico historialMedico) {
+    @Transactional
+    public void Eliminar(Long id) {
+        histMedRep.findById(id).ifPresentOrElse(histMedRep::delete,
+                () -> {
+                    throw new EntityNotFoundException("Historial médico no encontrado con ID: " + id);
+                });
+    }
+
+    /**
+     * Actualiza parcialmente un historial médico existente en la base de datos.
+     *
+     * @param id      ID del historial médico a actualizar.
+     * @param cambios Mapa que contiene los cambios a aplicar al historial médico.
+     * @return El historial médico actualizado parcialmente.
+     * @throws EntityNotFoundException Si no se encuentra el historial médico con el ID especificado.
+     */
+    @Override
+    @Transactional
+    public HistorialMedico actualizarParcial(Long id, Map<String, Object> cambios) {
+        Optional<HistorialMedico> historialMedicoOptional = histMedRep.findById(id);
+        if (historialMedicoOptional.isEmpty()) {
+            throw new EntityNotFoundException("Historial médico no encontrado con ID: " + id);
+        }
+
+        HistorialMedico historialMedico = historialMedicoOptional.get();
+        aplicarCambiosParciales(historialMedico, cambios);
+        return histMedRep.save(historialMedico);
+    }
+
+    // Métodos privados
+
+    private Optional<HistorialMedico> obtenerHistorialMedicoExistente(Long id) {
+        return histMedRep.findById(id);
+    }
+
+    private void asignarValoresPredeterminados(HistorialMedico historialMedico) {
         if (historialMedico.getAlergias() == null) {
             historialMedico.setAlergias("Ninguno");
         }
@@ -50,44 +133,17 @@ public class HistorialMedicoServiceImp implements HistorialMedicoService {
         if (historialMedico.getMedicamentos() == null) {
             historialMedico.setMedicamentos("Ninguno");
         }
-
-        return histMedRep.save(historialMedico);
     }
 
-    @Override
-    @Transactional
-    public HistorialMedico Actualizar(Long id, HistorialMedico historialMedico)  {
-        Optional<HistorialMedico> historialMedicoOptional = obtenerHistorialMedicoExistente(id);
-        if (historialMedicoOptional.isPresent()) {
-            HistorialMedico historialMedicoExistente = historialMedicoOptional.get();
-            actualizarCamposHistorialMedico(historialMedicoExistente, historialMedico);
-            return histMedRep.save(historialMedicoExistente);
-        } else {
-            throw new EntityNotFoundException();
-        }
+    private void actualizarCamposHistorialMedico(HistorialMedico historialMedicoExistente, HistorialMedico historialMedico) {
+        historialMedicoExistente.setDiagnostico(historialMedico.getDiagnostico());
+        historialMedicoExistente.setAlergias(historialMedico.getAlergias());
+        historialMedicoExistente.setCirugias(historialMedico.getCirugias());
+        historialMedicoExistente.setEnfermedadesPrevias(historialMedico.getEnfermedadesPrevias());
+        historialMedicoExistente.setMedicamentos(historialMedico.getMedicamentos());
     }
 
-    @Override
-    @Transactional
-    public void Eliminar(Long id) {
-        Optional<HistorialMedico> historialMedicoOptional = histMedRep.findById(id);
-        if (historialMedicoOptional.isPresent()) {
-            HistorialMedico historialMedico = historialMedicoOptional.get();
-            histMedRep.delete(historialMedico);
-        } else {
-            throw new EntityNotFoundException();
-        }
-    }
-
-    @Override
-    @Transactional
-    public HistorialMedico actualizarParcial(Long id, Map<String, Object> cambios) {
-        Optional<HistorialMedico> historialMedicoOptional = histMedRep.findById(id);
-        if (historialMedicoOptional.isEmpty()) {
-            throw new EntityNotFoundException();
-        }
-
-        HistorialMedico historialMedico = historialMedicoOptional.get();
+    private void aplicarCambiosParciales(HistorialMedico historialMedico, Map<String, Object> cambios) {
         cambios.forEach((key, value) -> {
             switch (key) {
                 case "diagnostico":
@@ -106,29 +162,8 @@ public class HistorialMedicoServiceImp implements HistorialMedicoService {
                     historialMedico.setMedicamentos((String) value);
                     break;
                 default:
-                    throw new IllegalArgumentException();
+                    throw new IllegalArgumentException("Campo desconocido: " + key);
             }
         });
-
-        return histMedRep.save(historialMedico);
-    }
-
-    private Optional<HistorialMedico> obtenerHistorialMedicoExistente(Long id) {
-        return histMedRep.findById(id);
-    }
-
-    private void actualizarCamposHistorialMedico(HistorialMedico historialMedicoExistente, HistorialMedico historialMedico) {
-        if (historialMedico.getDiagnostico().isEmpty() ||
-                historialMedico.getAlergias().isEmpty() ||
-                historialMedico.getCirugias().isEmpty() ||
-                historialMedico.getEnfermedadesPrevias().isEmpty() ||
-                historialMedico.getMedicamentos().isEmpty()) {
-            // Manejar el caso en el que algún campo requerido esté vacío
-        }
-        historialMedicoExistente.setDiagnostico(historialMedico.getDiagnostico());
-        historialMedicoExistente.setAlergias(historialMedico.getAlergias());
-        historialMedicoExistente.setCirugias(historialMedico.getCirugias());
-        historialMedicoExistente.setEnfermedadesPrevias(historialMedico.getEnfermedadesPrevias());
-        historialMedicoExistente.setMedicamentos(historialMedico.getMedicamentos());
     }
 }
