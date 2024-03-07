9a9d642802d0bb67c202a7def046d77702296b7c
Angel Arribasplata <escobar1234123@gmail.com>
2024-03-07 18:33:22 -0500

    creando Rest de todos las clases y agregando DTO´s de todas las clases

---
+++ src/main/java/CGCS/COM/ProyectoFinalSWIIISGCS/DTO/AdministrativoDTO.java
@@ -0,0 +1,15 @@
+package CGCS.COM.ProyectoFinalSWIIISGCS.DTO;
+
+import lombok.Data;
+
+@Data
+public class AdministrativoDTO {
+    private Long idAdministrativo;
+    private String nombres;
+    private String apellidos;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String cargo;
+
+}
